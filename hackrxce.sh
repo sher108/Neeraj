cd7150be33e8592ae0653c76f4cc8b689a4a18fa
sher108 <88870961+sher108@users.noreply.github.com>
2021-08-13 16:59:43 +0530

    Update hackrxce.sh

--- hackrxce.sh
+++ hackrxce.sh
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm hackrxce.py
-wget -q "https://raw.githubusercontent.com/sher108/Neeraj/main/hackrxce.py"
+rm hackrxce.py hackrxce1.py
+wget -q "https://raw.githubusercontent.com/sher108/Neeraj/main/hackrxce1.py"
 clear
-python hackrxce.py
+python hackrxce1.py
